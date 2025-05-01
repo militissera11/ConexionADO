61a3a2652270a6e25d33512c8b52624f9625dc6e
Milagros <Milagros@DESKTOP-61FABFD>
2025-05-01 20:22:50 -0300

    Agregar archivos de proyecto.

---
+++ pryTisseraConexionBD/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace pryTisseraConexionBD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(52, 71);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.Size = new System.Drawing.Size(370, 218);
+            this.dgvProductos.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvProductos);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvProductos;
+    }
+}
+
---
+++ pryTisseraConexionBD/conexion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+
+
+
+
+namespace pryTisseraConexionBD
+{
+    internal class conexion
+    {
+        public void Conectar(DataGridView dgv)
+        {
+            string connectionString = "Server=localhost;Database=Comercio;Trusted_Connection=True;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show("✅ Conexión exitosa a la base de datos.");
+
+                    // CONSULTA SQL
+                    string query = "SELECT * FROM Productos";
+
+                    // CREA ADAPTADOR Y LLENA LA TABLA
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+
+                    // MUESTRA LOS DATOS 
+                    dgv.DataSource = table;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("❌ Error al conectar: " + ex.Message);
+                }
+            }
+        }
+
+    }
+
+
+}
